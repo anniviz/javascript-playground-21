48b22376e3a7e75125171d68a9f534c0f608d60a
Annemarie Wisbar <a.wisbar@gmail.com>
2021-04-27 15:26:45 +0200

    Add navigation buttons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-console.clear()
-const box = document.querySelector('.box')
-const buttonRotate = document.querySelector('.button-rotate')
-const buttonBackground = document.querySelector('.button-background')
-const buttonRadius = document.querySelector('.button-radius')
+const homePage = document.querySelector('.home')
+const bookmarksPage = document.querySelector('.bookmarks')
+const createPage = document.querySelector('.create')
+const profilePage = document.querySelector('.profile')
 
-buttonRotate.addEventListener('click', () => {
-  box.classList.toggle('rotate')
+const buttonHome = document.querySelector('.button-home')
+const buttonBookmarks = document.querySelector('.button-bookmarks')
+const buttonCreate = document.querySelector('.button-create')
+const buttonProfile = document.querySelector('.button-profile')
+
+buttonHome.addEventListener('click', () => {
+  homePage.classList.remove('hidden')
+  bookmarksPage.classList.add('hidden')
+  createPage.classList.add('hidden')
+  profilePage.classList.add('hidden')
+
+  buttonHome.classList.add('active')
+  buttonBookmarks.classList.remove('active')
+  buttonCreate.classList.remove('active')
+  buttonProfile.classList.remove('active')
 })
 
-buttonBackground.addEventListener('click', () => {
-  box.classList.toggle('blue')
+buttonBookmarks.addEventListener('click', () => {
+  homePage.classList.add('hidden')
+  bookmarksPage.classList.remove('hidden')
+  createPage.classList.add('hidden')
+  profilePage.classList.add('hidden')
+
+  buttonHome.classList.remove('active')
+  buttonBookmarks.classList.add('active')
+  buttonCreate.classList.remove('active')
+  buttonProfile.classList.remove('active')
 })
 
-buttonRadius.addEventListener('click', () => {
-  box.classList.toggle('radius')
+buttonCreate.addEventListener('click', () => {
+  homePage.classList.add('hidden')
+  bookmarksPage.classList.add('hidden')
+  createPage.classList.remove('hidden')
+  profilePage.classList.add('hidden')
+
+  buttonHome.classList.remove('active')
+  buttonBookmarks.classList.remove('active')
+  buttonCreate.classList.add('active')
+  buttonProfile.classList.remove('active')
+})
+
+buttonProfile.addEventListener('click', () => {
+  homePage.classList.add('hidden')
+  bookmarksPage.classList.add('hidden')
+  createPage.classList.add('hidden')
+  profilePage.classList.remove('hidden')
+
+  buttonHome.classList.remove('active')
+  buttonBookmarks.classList.remove('active')
+  buttonCreate.classList.remove('active')
+  buttonProfile.classList.add('active')
 })
