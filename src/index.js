638231410830d30146ec2c83e755611826783891
Annemarie Wisbar <a.wisbar@gmail.com>
2021-04-27 12:47:09 +0200

    Add rotate function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 console.clear()
 const box = document.querySelector('.box')
-const buttonAdd = document.querySelector('.button-add')
-const buttonRemove = document.querySelector('.button-remove')
-const buttonToggle = document.querySelector('.button-toggle')
+const buttonRotate = document.querySelector('.button-rotate')
 
-buttonAdd.addEventListener('click', () => {
-  box.classList.add('blue')
-})
-
-buttonRemove.addEventListener('click', () => {
-  box.classList.remove('blue')
-})
-
-buttonToggle.addEventListener('click', () => {
-  box.classList.toggle('blue')
+buttonRotate.addEventListener('click', () => {
+  console.log('test')
+  box.classList.toggle('rotate')
 })
