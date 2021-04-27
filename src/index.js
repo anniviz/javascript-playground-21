cf4291561355beb07937fe23d4df0a0cd03100df
Annemarie Wisbar <a.wisbar@gmail.com>
2021-04-27 12:52:17 +0200

    Add border radius function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ console.clear()
 const box = document.querySelector('.box')
 const buttonRotate = document.querySelector('.button-rotate')
 const buttonBackground = document.querySelector('.button-background')
+const buttonRadius = document.querySelector('.button-radius')
 
 buttonRotate.addEventListener('click', () => {
   box.classList.toggle('rotate')
@@ -9,4 +10,8 @@ buttonRotate.addEventListener('click', () => {
 
 buttonBackground.addEventListener('click', () => {
   box.classList.toggle('blue')
+})
+
+buttonRadius.addEventListener('click', () => {
+  box.classList.toggle('radius')
 })
