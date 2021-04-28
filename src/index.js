60323256857156b7aeab620ba3d4aaa6efb65c00
Annemarie Wisbar <a.wisbar@gmail.com>
2021-04-28 15:06:57 +0200

    Exercise switch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const form = document.querySelector('[data-form]')
-const box = document.querySelector('[data-box]')
-const input = document.querySelector('[name="class-text"]')
-const output = document.querySelector('[data-output]')
+const input1 = document.querySelector('[name="class-text1"]')
+const input2 = document.querySelector('[name="class-text2"]')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
-  box.className = 'box ' + input.value
-  output.innerText = input.value.toUpperCase()
-  input.value = ''
-  input.focus()
+  const input1Text = input1.value
+  const input2Text = input2.value
+
+  console.log(input2Text)
+  input1.value = input2Text.toUpperCase()
+  input2.value = input1Text.toLowerCase()
 })
