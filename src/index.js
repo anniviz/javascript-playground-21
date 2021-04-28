7a013c8ed616ce0ff2ab1e8fdf6c4b0a86cb016b
Annemarie Wisbar <a.wisbar@gmail.com>
2021-04-28 14:38:03 +0200

    Third exercise

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const form = document.querySelector('[data-form]')
 const box = document.querySelector('[data-box]')
 const input = document.querySelector('[name="class-text"]')
+const output = document.querySelector('[data-output]')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
   box.className = 'box ' + input.value
+  output.innerText = input.value.toUpperCase()
   input.value = ''
   input.focus()
 })
