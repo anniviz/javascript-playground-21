24b07668326666c74d62aa3f183319105e311f0a
Annemarie Wisbar <a.wisbar@gmail.com>
2021-04-28 12:50:22 +0200

    First exercise

--- src/index.js
+++ src/index.js
@@ -1,57 +1,10 @@
-const homePage = document.querySelector('.home')
-const bookmarksPage = document.querySelector('.bookmarks')
-const createPage = document.querySelector('.create')
-const profilePage = document.querySelector('.profile')
+const form = document.querySelector('[data-form]')
+const box = document.querySelector('[data-box]')
+const input = document.querySelector('[name="class-text"]')
 
-const buttonHome = document.querySelector('.button-home')
-const buttonBookmarks = document.querySelector('.button-bookmarks')
-const buttonCreate = document.querySelector('.button-create')
-const buttonProfile = document.querySelector('.button-profile')
-
-buttonHome.addEventListener('click', () => {
-  homePage.classList.remove('hidden')
-  bookmarksPage.classList.add('hidden')
-  createPage.classList.add('hidden')
-  profilePage.classList.add('hidden')
-
-  buttonHome.classList.add('active')
-  buttonBookmarks.classList.remove('active')
-  buttonCreate.classList.remove('active')
-  buttonProfile.classList.remove('active')
-})
-
-buttonBookmarks.addEventListener('click', () => {
-  homePage.classList.add('hidden')
-  bookmarksPage.classList.remove('hidden')
-  createPage.classList.add('hidden')
-  profilePage.classList.add('hidden')
-
-  buttonHome.classList.remove('active')
-  buttonBookmarks.classList.add('active')
-  buttonCreate.classList.remove('active')
-  buttonProfile.classList.remove('active')
-})
-
-buttonCreate.addEventListener('click', () => {
-  homePage.classList.add('hidden')
-  bookmarksPage.classList.add('hidden')
-  createPage.classList.remove('hidden')
-  profilePage.classList.add('hidden')
-
-  buttonHome.classList.remove('active')
-  buttonBookmarks.classList.remove('active')
-  buttonCreate.classList.add('active')
-  buttonProfile.classList.remove('active')
-})
-
-buttonProfile.addEventListener('click', () => {
-  homePage.classList.add('hidden')
-  bookmarksPage.classList.add('hidden')
-  createPage.classList.add('hidden')
-  profilePage.classList.remove('hidden')
-
-  buttonHome.classList.remove('active')
-  buttonBookmarks.classList.remove('active')
-  buttonCreate.classList.remove('active')
-  buttonProfile.classList.add('active')
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  box.className = 'box ' + input.value
+  input.value = ''
+  input.focus()
 })
