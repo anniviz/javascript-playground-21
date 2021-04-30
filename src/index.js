8eb6b15a1f3a112e852a9c2c1da9cf72d972c636
Annemarie Wisbar <a.wisbar@gmail.com>
2021-04-30 10:17:40 +0200

    Add functions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-const form = document.querySelector('[data-form]')
+const form1 = document.querySelector('[data-form1]')
+const form2 = document.querySelector('[data-form2]')
 const input1 = document.querySelector('[name="class-text1"]')
 const input2 = document.querySelector('[name="class-text2"]')
+const input3 = document.querySelector('[name="class-text3"]')
+const input4 = document.querySelector('[name="class-text4"]')
 
-form.addEventListener('submit', event => {
+form1.addEventListener('submit', event => {
   event.preventDefault()
-  const input1Text = input1.value
-  const input2Text = input2.value
-
-  console.log(input2Text)
-  input1.value = input2Text.toUpperCase()
-  input2.value = input1Text.toLowerCase()
+  switchText(input1, input2)
 })
+
+form2.addEventListener('submit', event => {
+  event.preventDefault()
+  switchText(input3, input4)
+})
+
+function switchText(firstInput, secondInput) {
+  const text1 = firstInput.value
+  const text2 = secondInput.value
+  firstInput.value = text2
+  secondInput.value = text1
+}
